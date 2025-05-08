1621da5c3075eae95b0cdeedbe0084561e88f811
Hengtong-sou <souhengtong480@gmail.com>
2025-05-08 16:30:52 +0700

    start

---
+++ Modules/Dashboard/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('base')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('dashboard.name') !!}
+    </p>
+@endsection
---
+++ resources/views/base.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Modulize</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <style>
+            body {
+                font-family: 'Nunito';
+            }
+        </style>
+        @livewireStyles
+    </head>
+    <body class="antialiased">
+        <div class="">
+
+          <div class="header p-3 shadow-lg flex items-center">
+            <img src="{{ asset('lets-go.png') }}" alt="" class="w-12 h-12">
+          </div>
+          <div class="">  @yield('content') </div>
+
+        </div>
+        @livewireScripts
+    </body>
+</html>
